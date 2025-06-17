c98aec75b08292e7f8cc62dc8587acea07ab0a05
Abhiram <113409577+ABHCI15@users.noreply.github.com>
2025-06-17 01:15:03 +0000

    update model to gemini-2.5-flash-preview-05-20

--- GeminiGoogle.py
+++ GeminiGoogle.py
@@ -7,7 +7,7 @@ st.sidebar.success("Please select version above ")
 
 gemini.configure(api_key=st.secrets["api_key"])
 models = [m for m in gemini.list_models() if 'generateText' in m.supported_generation_methods]
-model = gemini.GenerativeModel('gemini-exp-1206')
+model = gemini.GenerativeModel('gemini-2.5-flash-preview-05-20')
 
 
 temp = 0.7
